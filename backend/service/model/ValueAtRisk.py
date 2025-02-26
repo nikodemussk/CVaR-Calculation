dfff4ff1538a28b9797886f63a76aa2ac78cb483
Niko <nikodemus.stanley@gmail.com>
2025-02-26 20:56:18 +0000

    --amend

---
+++ backend/service/model/ValueAtRisk.py
@@ -0,0 +1,4 @@
+class ValueAtRisk: 
+    def __init__(self, conditionalValueAtRisk, valueAtRisk):
+        self.conditionalValueAtRisk = conditionalValueAtRisk
+        self.valueAtRisk = valueAtRisk
